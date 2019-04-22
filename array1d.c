21a7484116d1341054f34bee5e01ce1cd61148e5
thekunalsaini <43953836+thekunalsaini@users.noreply.github.com>
2019-04-22 23:01:59 +0530

    GIT TERMINAL
    These files are uploaded from local git using Ubuntu system using cmds like git init , git status for status, etc.

---
+++ array1d.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a[10]={2},sum=0;
+int i;
+printf("enter 10 values");
+//for(i=0;i<=9;i++)
+//scanf("%s",&a[i]);
+
+for(i=0;i<=9;i++)
+printf("%d",a[i]);//it means in char only we can use name of array to print otherwise we get garbage value
+
+/*avg=sum/10.0;
+printf("average is %f",avg);*/
+getch();
+}
